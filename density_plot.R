3d3058f55c9c06f47cb3faee2612063fc297f424
Taylor Mackay <mackaytc@users.noreply.github.com>
2018-04-18 10:14:41 -0700

    density plot + markdown guide

---
+++ density_plot.R
@@ -0,0 +1,30 @@
+################################################################################
+# Basic Density Plot Using ggplot2
+################################################################################
+
+# Basic ggplot-related packages
+
+library(ggplot2)
+library(latex2exp)
+
+rm(list = ls())
+
+# Diamonds samples dataset has prices and other attributes of ~54,000 diamonds.
+
+data(diamonds)
+
+################################################################################
+# Plotting
+################################################################################
+
+# Setting options for plot formatting, including font type + size, and title
+# alignment, using `minimal` theme
+
+theme_bcg <- theme_minimal(base_size = 9, base_family = "Palatino") + 
+  theme(plot.title = element_text(hjust = 0.5))
+
+ggplot(data = diamonds, aes(x = depth)) + geom_density() + 
+  labs(title = "Sample Density Plot", 
+       y = "Density", 
+       x = "Depth") + 
+  theme_bcg
